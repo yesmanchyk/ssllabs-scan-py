1fd20ef269032def62c3bfb9bfd3a2cc9ef6979c
sasha <esmanchik@gmail.com>
2024-04-20 21:03:38 -0400

    Added ArgumentParser

--- ssllabs-scan.py
+++ ssllabs-scan.py
@@ -2,14 +2,23 @@ from ssllabs.scan import Scanner, Client
 import asyncio
 import sys
 import json
+import argparse
 
 async def main():
-    if len(sys.argv) < 4:
-        print('Usage: python ssllabs-scan.py <email> <host> <report>')
-    else:    
-        s = Scanner(Client(), 'https://api.ssllabs.com/api/v4')
-        a = await s.analyze(sys.argv[1], sys.argv[2])
-        await s.save_report(a, sys.argv[3])
+
+    parser = argparse.ArgumentParser(description='Scan SSL hosts')
+    parser.add_argument('hosts', metavar='H', type=str, 
+                        nargs='+',
+                        help='host to scan')
+    parser.add_argument('-e', '--email', required=True,
+                        help='authentication email')
+    parser.add_argument('-r', '--report', required=True,
+                        help='report file name')
+
+    args = parser.parse_args()
+    s = Scanner(Client(), 'https://api.ssllabs.com/api/v4')
+    a = await s.analyze(args.email, args.hosts[0])
+    await s.save_report(a, args.report)
 
 if __name__ == "__main__":
     asyncio.run(main())
